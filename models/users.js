418bb868ae17c071f7575ff6d175ee1bd7725d04
MedoHaytham <104214885+MedoHaytham@users.noreply.github.com>
2026-07-03 01:28:45 +0300

    feat: add User model with authentication methods and implement APIFeatures utility class for database queries

--- models/users.js
+++ models/users.js
@@ -53,6 +53,10 @@ const userSchema = new mongoose.Schema({
     type: Boolean,
     default: true,
     select: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
 },{
   toJSON: {virtuals: true},
