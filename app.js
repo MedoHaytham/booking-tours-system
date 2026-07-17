a33b38d6957cb1f03e837135a93f9717fba4daed
MedoHaytham <104214885+MedoHaytham@users.noreply.github.com>
2026-07-17 15:48:34 +0300

    feat: enable rate limiting on API routes to prevent abuse

--- app.js
+++ app.js
@@ -74,12 +74,12 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Limit requests from same IP
-// const limiter = rateLimit({
-//   max: 100,
-//   windowMs: 60 * 60 * 1000,
-//   message: 'Too many requests from this IP, please try again in an hour!'
-// });
-// app.use('/api', limiter);
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
 
 // booking checkout webhook ( this must come before all other routes ) ( /raw not json ) ( because it will be sent from stripe )
 app.post('/webhook-checkout', 
